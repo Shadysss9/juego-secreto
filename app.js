7e7d42549c1cda99d1726c6167ede013d151f9b4
Shadysss9 <shadysb94@gmail.com>
2024-08-15 23:33:31 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function mostrarTablaDeMultiplicar(numero) {
+    for (var i = 1; i <= 10; i++) {
+      var resultado = numero * i;
+      console.log(numero + " x " + i + " = " + resultado);
+    }
+  }
+  // Ejemplo de uso
+  let numero = 7;
+  mostrarTablaDeMultiplicar(numero);
